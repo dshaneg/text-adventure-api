964d11e15f1b13d4fe559cdf98900c00465a7162
Shane Gibbons <shane.gibbons@gmail.com>
2018-02-10 02:05:20 -0600

    First working "hello world" using core package

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,35 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
-import { gameManager, gameEngine } from '@dshaneg/text-adventure-core';
+import { TextAdventureCore as Core } from '@dshaneg/text-adventure-core';
+
+// repositories
+const gameDefinitionRepository = new Core.defaultImplementations.GameDefinitionRepository();
+const mapNodeRepository = new Core.defaultImplementations.MapNodeRepository();
+const itemRepository = new Core.defaultImplementations.ItemRepository();
+
+const gameSessionRepository = new Core.defaultImplementations.GameSessionRepositoryMem();
 
 export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
 
-  const sessionToken = gameManager.createGame();
+  const gameState = Core.createGameManager(gameSessionRepository).createGame();
+  const gameEngine = Core.createGameEngine(gameDefinitionRepository, mapNodeRepository, itemRepository, true);
+
+  const startResponse = gameEngine.startGame(gameState);
+
+  const availableDirections = gameState.queryAvailableDirections(mapNodeRepository.gameMap);
+  const currentNode = gameState.player.currentNode;
+
+  const responseBody = {
+    command: startResponse.command,
+    locName: currentNode.name,
+    locDescription: currentNode.description,
+    locVisited: gameState.player.visited(currentNode),
+    events: startResponse.events,
+    moves: availableDirections
+  };
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ sessionToken }),
+    body: JSON.stringify(responseBody),
     input: event
   };
 
